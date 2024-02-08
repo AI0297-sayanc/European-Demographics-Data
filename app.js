ca80346174a0d6c5cb55339c42a5c882433dcd02
Achintya Gopal Mondal <achintyam@aggregateintelligence.in>
2024-02-08 19:06:14 +0530

    code clean up

--- app.js
+++ app.js
@@ -42,13 +42,7 @@ app.use((req, res, next) => {
 // error handler
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get("env") === "development" ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render("error")
+  res.status(err.status || 500).json({ error: true, message: err.message })
 })
 
 module.exports = app
