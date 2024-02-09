f1da8f4472e1b60f1edb11be28dd7b0f271579c3
Rishi Prakash Mishra <rishiprakash@aggregateintelligence.in>
2024-02-09 14:40:37 +0530

    Express-jwt middleware removed

--- routes/rest/index.js
+++ routes/rest/index.js
@@ -1,11 +1,6 @@
 const express = require("express")
 const router = express.Router()
 
-
-const { expressjwt } = require("express-jwt")
-
-const checkJwt = expressjwt({ secret: process.env.SECRET, algorithms: ["HS256"] }) // the JWT auth check middleware
-
 const test = require("./test")
 
 router.post("/test", test.test)
