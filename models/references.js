0308074aa652ee2c3dad974777392215b8243166
AI-0537-arpitadey <arpitadey@aggregateintelligence.in>
2024-02-12 11:32:19 +0530

    add new model named references and remove cencusattr

---
+++ models/references.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose")
+const mongoosePaginate = require("mongoose-paginate-v2")
+
+const ReferenceSchema = new mongoose.Schema({
+
+  attribute: {
+    type: String,
+    required: true,
+  },
+
+  name: {
+    type: String,
+    required: true,
+  },
+
+  levelCode: {
+    type: Number,
+    required: true,
+  },
+
+  countryCode: {
+    type: String,
+    required: true,
+  },
+
+  date: {
+    type: String,
+    default: null,
+  },
+  source: {
+    type: String,
+    default: null,
+  },
+  sourceName: {
+    type: String,
+    default: null,
+  },
+  description: String,
+  currencyCode: {
+    type: String,
+    default: null,
+  }
+
+})
+
+ReferenceSchema.set("timestamps", true)
+ReferenceSchema.set("toJSON", { virtuals: true })
+ReferenceSchema.set("toObject", { virtuals: true })
+ReferenceSchema.plugin(mongoosePaginate)
+
+module.exports = mongoose.model("Reference", ReferenceSchema)
