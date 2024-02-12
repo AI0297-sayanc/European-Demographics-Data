2352eed0d1a3898c5911d002f496772f5fabe7b8
AI0564-swati <swati@aggregateintelligence.in>
2024-02-12 14:31:43 +0530

    Implemented authentication for test route.

--- routes/rest/index.js
+++ routes/rest/index.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
+const { authMiddleware } = require("../../middlewares")
 
 const test = require("./test")
 
-router.post("/test", test.test)
+router.post("/test", authMiddleware, test.test)
 
 module.exports = router
