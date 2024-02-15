f16ff6e6cee6020be69933a2e0733e3b02aaf1a6
AI-0537-arpitadey <arpitadey@aggregateintelligence.in>
2024-02-15 12:38:36 +0530

    added api doc for /countries

--- routes/rest/References/index.js
+++ routes/rest/References/index.js
@@ -78,7 +78,29 @@ module.exports = {
       return res.status(500).json({ error: true, message: error.message })
     }
   },
-
+  /**
+ * @api {get} /countries Retrieve Countries with countrycode and countryname
+ * @apiName Retrieve Countries
+ * @apiGroup References
+ * @apiVersion 1.0.0
+ * @apiParam {Number} [page=1] Page number for pagination. Default: 1.
+ * @apiParam {Number} [size=10] Number of items per page. Default: 10.
+ *
+* @apiSuccessExample {json} Success-Response:200
+    {
+    "error": false,
+    "countries": [
+        {
+            "countryCode": "AT",
+            "countryName": "Austria"
+        }
+    ],
+    "totalData": 25,
+    "totalPages": 25,
+    "page": 1,
+    "size": 1
+}
+*/
   async countries(req, res) {
     const { page = 1, size = 10 } = req.query
     try {
