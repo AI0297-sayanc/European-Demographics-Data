5f66bff3ce65dbba5c3e8aab0c6c4f8a0835c5f1
Achintya Gopal Mondal <achintyam@aggregateintelligence.in>
2024-02-20 13:33:30 +0530

    change routes to search/radius, add count field in resp, add levelcodes validation

--- routes/rest/index.js
+++ routes/rest/index.js
@@ -4,12 +4,17 @@ const { authMiddleware } = require("../../middlewares")
 
 const Reference = require("./References")
 const regionSearchRadius = require("./Regions Search")
+const demographicDataRadius = require("./Demographic Data")
 // const test = require("./test")
 
 // router.post("/test", test.test)
 
+// Demographic Data routes...
+router.post("/demographics/radius", demographicDataRadius.byRadius)
+
 // Regions Search routes....
-router.post("/regionssearch/radius", regionSearchRadius.byRadius)
+router.post("/search/radius", regionSearchRadius.byRadius)
+
 // route for References
 router.get("/references/attributes", Reference.attributes)
 router.get("/references/levels/:countryCode", Reference.levelsByCountryCode)
