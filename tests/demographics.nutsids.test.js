c9dc8e53078405add49f6f36f2e2ed5a57e2a21e
AI0564-swati <swati@aggregateintelligence.in>
2024-02-21 14:09:57 +0530

    further test cases

--- tests/demographics.nutsids.test.js
+++ tests/demographics.nutsids.test.js
@@ -13,6 +13,13 @@ test.after.always(teardownMongo)
 test.beforeEach(setupFixtures)
 test.afterEach(teardownFixtures)
 
+const requestBody = {
+  nutsIds: ["DK03", "DK04", "DK05", "AT06"],
+  countryCode: "DK",
+  levelCode: 1,
+  censusAttributes: ["EU_E001", "EU_E002", "EU_E003", "EU_E004", "EU_E005"]
+}
+
 test.serial("Validating Response Schema", async (t) => {
   const schema = Joi.object({
     error: Joi.boolean().required(),
@@ -26,20 +33,45 @@ test.serial("Validating Response Schema", async (t) => {
     ).required()
   })
 
-  const requestBody = {
-    nutsIds: ["DK03", "DK04", "DK05", "AT06"],
-    countryCode: "DK",
-    levelCode: 1,
-    censusAttributes: ["EU_E001", "EU_E002", "EU_E003", "EU_E004", "EU_E005"]
-  }
-
   const response = await request(app)
     .post("/api/v1/demographics/nutsids")
     .set("Accept", "application/json")
     .send(requestBody)
 
-  t.is(response.status, 200, `Expected 200 OK, but got ${response.status}`)
+  t.is(response.status, 200)
 
   const { error } = schema.validate(response.body, { abortEarly: false })
   t.is(error === undefined, true, error?.message)
+})
+
+test.serial("Checking if nutsIds is null", async (t) => {
+  const response = await request(app)
+    .post("/api/v1/demographics/nutsids")
+    .set("Accept", "application/json")
+    .send({ ...requestBody, nutsIds: null })
+  t.is(response.status, 400)
+})
+
+test.serial("Checking if nutsIds is undefined", async (t) => {
+  const response = await request(app)
+    .post("/api/v1/demographics/nutsids")
+    .set("Accept", "application/json")
+    .send({ ...requestBody, nutsIds: undefined })
+  t.is(response.status, 400)
+})
+
+test.serial("Checking if nutsIds is string", async (t) => {
+  const response = await request(app)
+    .post("/api/v1/demographics/nutsids")
+    .set("Accept", "application/json")
+    .send({ ...requestBody, nutsIds: "DK03" })
+  t.is(response.status, 400)
+})
+
+test.serial("Checking if nutsIds is empty array", async (t) => {
+  const response = await request(app)
+    .post("/api/v1/demographics/nutsids")
+    .set("Accept", "application/json")
+    .send({ ...requestBody, nutsIds: [] })
+  t.is(response.status, 400)
 })
